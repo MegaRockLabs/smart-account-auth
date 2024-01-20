a665344d2ee6e6b96f340a5ae82df3f5f3ce46e1
Martin Kromsten <kromsten@pm.me>
2024-01-20 16:35:56 +0100

    ids helpers

--- packages/bundle/src/lib.rs
+++ packages/bundle/src/lib.rs
@@ -106,6 +106,29 @@ impl CredentialData {
         }
     }
 
+    pub fn primary_id(&self) -> CredentialId {
+        self.primary().id()
+    }
+
+
+    pub fn secondary_ids(&self) -> Vec<CredentialId> {
+        if self.primary_index.is_some() {
+            self.credentials
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| *i != self.primary_index.unwrap() as usize)
+                .map(|(_, c)| c.id())
+                .collect()
+        } else {
+            match self.credentials.len() {
+                0 => return vec![],
+                1 => return vec![],
+                _ => self.credentials.iter().skip(1).map(|c| c.id()).collect()
+                
+            }
+        }
+    }
+
     pub fn ids(&self) -> Vec<CredentialId> {
         self.credentials.iter().map(|c| c.id()).collect()
     }
