9aec0afa38cf4a81559d3879143e856a7fc3a2a5
Martin Kromsten <kromsten@pm.me>
2024-08-05 19:30:07 +0200

    redefining public key

--- packages/custom/src/passkey/passkey.rs
+++ packages/custom/src/passkey/passkey.rs
@@ -48,11 +48,14 @@ pub struct ClientData {
 #[wasm_serde]
 pub struct PasskeyCredential {
     pub id                   :       Binary,
-    pub public_key           :       Binary,
     pub signature            :       Binary,
     pub authenticator_data   :       Binary,
     pub client_data          :       ClientData,
-    pub user_handle          :       Option<String>
+    /// Optional user handle reserved for future use
+    pub user_handle          :       Option<String>,
+    /// Public key is essential for verification but can be supplied on the contract side
+    /// and omitted by client
+    pub public_key           :       Option<Binary>,
 }
 
 
@@ -67,6 +70,7 @@ impl Verifiable for PasskeyCredential {
         ensure!(self.signature.len() > 0, AuthError::generic("Empty signature"));
         ensure!(self.client_data.challenge.len() > 0, AuthError::generic("Empty challenge"));
         ensure!(self.client_data.ty == "webauthn.get", AuthError::generic("Invalid client data type"));
+        ensure!(self.public_key.is_some(), AuthError::generic("Missing public key"));
         Ok(())
     }
 
@@ -81,7 +85,7 @@ impl Verifiable for PasskeyCredential {
         let res = secp256r1_verify(
             &hash,
             &self.signature,
-            &self.id
+            self.public_key.as_ref().unwrap()
         )?;
         ensure!(res, AuthError::generic("Signature verification failed"));
         Ok(())
@@ -94,11 +98,11 @@ impl Verifiable for PasskeyCredential {
         hasher.update(&self.authenticator_data);
         hasher.update(&client_data_hash);
         let hash = hasher.finalize();
-        
+
         let res = secp256r1_verify(
             &hash,
             &self.signature,
-            &self.id
+            self.public_key.as_ref().unwrap()
         )?;
         ensure!(res, AuthError::generic("Signature verification failed"));
         Ok(self.clone())
