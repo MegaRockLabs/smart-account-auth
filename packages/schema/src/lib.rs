4774be7d000eaca2f5900bd58e3b6315453b2b5e
Martin Kromsten <kromsten@pm.me>
2024-10-11 12:43:10 +0200

    fix version reference

--- packages/schema/src/lib.rs
+++ packages/schema/src/lib.rs
@@ -4,15 +4,6 @@ use macros_proto;
 
 pub use macros_proto::wasm_serde;
 
-#[macro_export]
-macro_rules! ensure {
-    ($cond:expr, $e:expr) => {
-        if !($cond) {
-            return Err(core::convert::From::from($e));
-        }
-    };
-}
-
 
 #[cfg(feature = "cosmwasm")]
 pub use {serde, schemars};
