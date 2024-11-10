b413a341fd614c163f36cf0dcb234eaddf7a8991
Martin Kromsten <kromsten@pm.me>
2024-11-10 06:09:01 +0100

    non-denied message field

--- packages/common/src/messages.rs
+++ packages/common/src/messages.rs
@@ -74,36 +74,27 @@ pub struct MsgDataToSign<M = String> {
 }
 
 
-#[derive(
-    Clone,
-    Debug,
-    PartialEq
-)]
-#[cfg_attr(feature = "cosmwasm", 
-    derive(
-        ::saa_schema::serde::Serialize,
-        ::saa_schema::serde::Deserialize,
-        ::saa_schema::schemars::JsonSchema
-    ),
-    schemars(crate = "::saa_schema::schemars")
-)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "cosmwasm", derive(
+    ::saa_schema::serde::Serialize,
+    ::saa_schema::serde::Deserialize,
+    ::saa_schema::schemars::JsonSchema
+), schemars(crate = "::saa_schema::schemars"
+))]
 #[cfg_attr(feature = "substrate", derive(
-    ::saa_schema::scale::Encode, 
-    ::saa_schema::scale::Decode
+    ::saa_schema::scale::Encode, ::saa_schema::scale::Decode
 ))]
 #[cfg_attr(feature = "solana", derive(
-    ::saa_schema::borsh::BorshSerialize, 
-    ::saa_schema::borsh::BorshDeserialize
+    ::saa_schema::borsh::BorshSerialize, ::saa_schema::borsh::BorshDeserialize
 ))]
-#[cfg_attr(all(feature = "std", feature="substrate"), derive(
-    saa_schema::scale_info::TypeInfo)
-)]
+#[cfg_attr(all(feature = "std", feature="substrate"), derive(saa_schema::scale_info::TypeInfo))]
 #[allow(clippy::derive_partial_eq_without_eq)]
 pub struct MsgDataToVerify {
     pub chain_id: String,
     pub contract_address: String,
     pub nonce: String,
 }
+
 
 impl<M> Into<MsgDataToVerify> for &MsgDataToSign<M> {
     fn into(self) -> MsgDataToVerify {
